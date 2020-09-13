f71fa7dc04b83d8b15a344a685d7b41ee04d3122
Stellar56 <arnaud.cholet90@sfr.fr>
2020-09-13 22:13:11 +0200

    add plugins linkcakebox

---
+++ plugins/linkLogs/init.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = getUser();
+$host = $_SERVER['HTTP_HOST'];
+
+require_once('conf.php');
+
+$optionlink = $onglet === true ? 'window.open':'window.location.replace';
+
+$jResult .= "plugin.url = '".$url."';";
+$jResult .= "plugin.optionlink = '".$optionlink."';";
+
+$theSettings->registerPlugin("linklogs");
+
+?>
