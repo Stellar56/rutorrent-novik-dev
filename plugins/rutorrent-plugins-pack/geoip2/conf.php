d69afe805339e652c2bcd15848f8429fe27d1af3
Stellar56 <arnaud.cholet90@sfr.fr>
2020-10-31 14:55:58 +0100

    add plugin geoip2

---
+++ plugins/rutorrent-plugins-pack/geoip2/conf.php
@@ -0,0 +1,13 @@
+<?php
+	// configuration parameters
+
+	$retrieveCountry = true;
+	$retrieveHost = true;
+	$retrieveComments = true;
+
+	// For retrieve hosts
+
+	$dnsResolver = '1.1.1.2';	// use gethostbyaddr, if null
+	$dnsResolverTimeout = 1;	// timeout in seconds
+
+?>
